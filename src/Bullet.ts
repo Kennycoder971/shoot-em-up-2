26c3f215d8e64a855b113274f16547da551debdf
kenny <kennycoder971@gmail.com>
2023-09-09 15:59:42 +0200

    Bullets

---
+++ src/Bullet.ts
@@ -0,0 +1,35 @@
+import Sprite from "./Sprite";
+
+export default class Bullet {
+  constructor(
+    public x: number,
+    public y: number,
+    public width: number,
+    public height: number,
+    public velocityX: number,
+    public velocityY: number,
+    public sprite: Sprite | null,
+    public speed: number,
+    public direction: "up" | "down"
+  ) {}
+
+  public draw(context: CanvasRenderingContext2D) {
+    if (!this.sprite) {
+      context.fillStyle = "#fff";
+
+      context.fillRect(this.x, this.y, this.width, this.height);
+      return;
+    }
+    this.sprite.draw(context);
+  }
+
+  public update() {
+    if (this.direction === "up") {
+      this.y -= this.speed;
+    }
+
+    if (this.direction === "down") {
+      this.y += this.speed;
+    }
+  }
+}
